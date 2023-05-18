6471c4355378f4491135e92f2249017907763ee3
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-17 22:10:57 -0400

    made the calculator screen layout

--- app/src/main/java/ca/tetervak/tipcalculator3/MainActivity.kt
+++ app/src/main/java/ca/tetervak/tipcalculator3/MainActivity.kt
@@ -5,18 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ca.tetervak.tipcalculator3.ui.theme.Pink500
 import ca.tetervak.tipcalculator3.ui.theme.TipCalculator3Theme
 
 class MainActivity : ComponentActivity() {
@@ -30,34 +44,162 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-private fun TipCalculator3App() {
+fun TipCalculator3App() {
     TipCalculator3Theme {
         // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-           CalculatorScreen()
+            CalculatorScreen()
         }
     }
 }
 
 @Composable
 fun CalculatorScreen() {
-   Column(
-       modifier = Modifier
-           .fillMaxSize()
-           .padding(32.dp),
-       horizontalAlignment = Alignment.CenterHorizontally,
-       verticalArrangement = Arrangement.spacedBy(32.dp)
-   ) {
-       Text(
-           text = "Tip Calculator",
-           fontSize = 24.sp,
-           color = Pink500
-       )
-   }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.tip_calculator_header),
+            fontSize = 24.sp,
+            color = colorResource(id = R.color.pink_500)
+        )
+        CalculatorInputs()
+        CalculatorOutputs()
+    }
 }
 
 
+@Composable
+fun CalculatorOutputs() {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.tip_amount_label),
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .sizeIn(minWidth = 112.dp)
+                        .wrapContentWidth(align = Alignment.End)
+                )
+                Text(
+                    text = "$18.00",
+                    fontSize = 20.sp,
+                    color = colorResource(id = R.color.purple_500)
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.bill_total_label),
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .sizeIn(minWidth = 112.dp)
+                        .wrapContentWidth(align = Alignment.End)
+                )
+                Text(
+                    text = "$118.00",
+                    fontSize = 20.sp,
+                    color = colorResource(id = R.color.purple_500)
+                )
+            }
+        }
+    }
 
+}
+
+@Composable
+fun CalculatorInputs() {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column {
+            CostOfServiceInput()
+            ServiceQualityInput()
+            RoundUpTipInput()
+        }
+
+    }
+}
+
+@Composable
+fun RoundUpTipInput() {
+    Row(
+        modifier = Modifier.padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.round_up_tip_input_label),
+            fontSize = 20.sp
+        )
+        Switch(
+            checked = true,
+            onCheckedChange = {},
+        )
+    }
+}
+
+@Composable
+fun ServiceQualityInput() {
+    Column(modifier = Modifier.padding(start = 16.dp, top = 16.dp)) {
+        Text(
+            text = stringResource(R.string.service_quality_input_label),
+            fontSize = 20.sp
+        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = false, onClick = { })
+            Text(
+                text = stringResource(id = R.string.quality_amazing_label),
+                fontSize = 18.sp
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = true, onClick = { })
+            Text(
+                text = stringResource(id = R.string.quality_good_label),
+                fontSize = 18.sp
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = false, onClick = { })
+            Text(
+                text = stringResource(id = R.string.quality_okay_label),
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostOfServiceInput() {
+    TextField(
+        label = { Text(text = stringResource(id = R.string.cost_of_service_label)) },
+        value = "100",
+        onValueChange = { },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true,
+        textStyle = TextStyle.Default.copy(
+            fontSize = 20.sp,
+            color = colorResource(id = R.color.purple_500)
+        ),
+        modifier = Modifier
+            .padding(16.dp)
+            .sizeIn(minWidth = 256.dp)
+    )
+}
