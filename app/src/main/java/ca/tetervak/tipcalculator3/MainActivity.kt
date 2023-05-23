58e1b995fa23dc35da470602bce02533d367bef8
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-23 08:35:16 -0400

    removed unused OptIn

--- app/src/main/java/ca/tetervak/tipcalculator3/MainActivity.kt
+++ app/src/main/java/ca/tetervak/tipcalculator3/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
@@ -229,7 +228,7 @@ fun ServiceQualityInput(serviceQuality: ServiceQuality, onChange: (ServiceQualit
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun ServiceCostInput(serviceCost: String, onChange: (String) -> Unit) {
     TextField(
